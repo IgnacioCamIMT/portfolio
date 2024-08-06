d5cee4d49a55aa8bf3ef27e4e13bef9b73f989c2
Ignacio Camacho <ignaciocamimt@gmail.com>
2024-08-06 00:12:48 -0400

    1st project commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'contact',
+        component: ContactComponent
+    }
+];
